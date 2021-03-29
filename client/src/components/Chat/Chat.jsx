9786646f49271ee27dca76a7fb079b48f9bbe870
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-03-29 11:54:32 +0500

    update chat page

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,32 +1,59 @@
-import React, { useState } from 'react';
-import {useParams} from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams, history, useHistory } from 'react-router-dom';
 
 import './Chat.css';
 
 const Chat = () => {
-
-	const {id} = useParams()
-	console.log(id) 
+	const { id } = useParams();
+	const history = useHistory();
+	// console.log(id, 'chat');
 
 	const [message, setMessage] = useState('');
-	const [postedMessages, setPostedMessages] = useState([]);
+	const userData = useSelector((state) => state.user.data);
+	const isLogged = useSelector((state) => state.user.isLogged);
+	// const userChats = userData.chats;
 
-	const [user, setUser] = useState('Dimon4ik-limon4ik');
+	console.log(userData);
+
+	if (userData) {
+		console.log(userData.chats);
+		// const userChat = userData.chats.filter((chat) => {
+		// console.log(chat);
+		// 	return chat['id'] === id;
+		// });
+		// console.log('chat', userChat);
+	}
+	// console.log(userChats);
+	// if (userChats) {
+	// 	for (const chat of userChats) {
+	// 		if (chat['id'] === id) {
+	// 			const ChatId = chat['id'];
+	// 			console.log('chat', userChats[ChatId]);
+	// 		}
+	// 	}
+	// }
+	// // console.log(userChat);
 
 	const sendMessage = (e) => {
 		e.preventDefault();
-		setPostedMessages((state) => [...state, message]);
-		setMessage('');
+		// setPostedMessages((state) => [...state, message]);
+		// setMessage('');
 	};
+
+	useEffect(() => {
+		if (!isLogged) {
+			history.push('/login');
+		}
+	}, [isLogged]);
+
 	return (
 		<div className='chat'>
 			<div className='chat__message'>
 				<div className='chat__message-area'>
-					{postedMessages?.map((message) => (
-						<div key={message}>
-							New message {message} from user {user}
-						</div>
-					))}
+					{/* {userChats?.map((messages) => (
+						<div key={messages.message}>New message {messages.message} from user</div>
+					))} */}
 					<br />
 				</div>
 				<textarea className='chat__message-input' type='text' placeholder='Введите сообщение' onChange={(e) => setMessage(e.target.value)} />

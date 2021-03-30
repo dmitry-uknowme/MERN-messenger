8fa3fce4951713fed485f97ea237f2ec4b3c41bf
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-03-30 13:53:39 +0500

    problems with update field DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,28 @@
-// import express, {Request,Response}  from 'express';
-// const app = express();
-// const server = require('http').Server(app);
-// const io = require('socket.io')(server);
-
-
-import express, {Request,Response}  from 'express';
-import { createServer } from "http";
-import { Server, Socket } from "socket.io"
+import express, { Request, Response } from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 const app = express();
-import {userTable} from './DB/schemas'
-import connectDB from './DB/connect'
+import { userTable } from './DB/schemas';
+import connectDB from './DB/connect';
 
-const server = createServer(app)
+const server = createServer(app);
 const io = new Server(server, {});
 
-const port:String|Number = process.env.PORT || 9000;
+const port: String | Number = process.env.PORT || 9000;
 
-connectDB()
+connectDB();
 
-io.on('connection',(socket:Socket)=>{
-	socket.on('sendMessage',(data:any)=>{
-		console.log(socket)
-	})
-	socket.on('test',(data:any)=>{
-		console.log('socket test',socket.id, data)
-	})
-})
+io.on('connection', (socket: Socket) => {
+	socket.on('sendMessage', (data: any) => {
+		console.log(socket);
+	});
+	socket.on('test', (data: any) => {
+		console.log('socket test', socket.id, data);
+	});
+});
 
-app.get('/users', async (req:Request, res:Response) => {
-	await userTable.find({}, (err:Error, result:JSON) => {
+app.get('/users', async (req: Request, res: Response) => {
+	await userTable.find({}, (err: Error, result: JSON) => {
 		if (err) {
 			console.log(err);
 		} else {
@@ -37,10 +31,10 @@ app.get('/users', async (req:Request, res:Response) => {
 	});
 });
 
-app.get('/users/:id', async (req:Request, res:Response) => {
+app.get('/users/:id', async (req: Request, res: Response) => {
 	const userId = req.params.id;
 	// console.log(userId);
-	await userTable.findOne({ id: userId }, (err:Error, result:JSON) => {
+	await userTable.findOne({ id: userId }, (err: Error, result: JSON) => {
 		if (err) {
 			console.log(err);
 		} else {
@@ -50,12 +44,28 @@ app.get('/users/:id', async (req:Request, res:Response) => {
 	console.log(req.body);
 });
 
-
+app.post('/users/add_message', async (req: Request, res: Response) => {
+	const { userId, chatId, message } = req.body;
+	// if (err) {
+	// 	console.log(err);
+	// } else {
+	// 	res.json(result);
+	// }
+	// await userTable.findOneAndUpdate({id:userId},{},(err:Error, result:JSON)=> {
+	// 		if(err)
+	// 		{
+	// 			console.log(err)
+	// 		}
+	// 		else {
+	// 			res.json(result);
+	// 		}
+	// });
+	// @ts-expect-error
+	// userTable.findOneAndUpdate({ id: userId }, { $push: { 'chats[chatId].messages': message } });
+	userTable.findOneAndUpdate({ id: 1 }, { $set: { 'chats.0.messages.0': 'new value' } });
+});
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
 
 const Dmitry = new userTable({
 	id: 1,
@@ -69,7 +79,6 @@ const Dmitry = new userTable({
 			messages: [
 				{ isMy: false, message: 'димасик можешь распечатать' },
 				{ isMy: true, message: 'Да' },
-				
 			],
 		},
 		{
